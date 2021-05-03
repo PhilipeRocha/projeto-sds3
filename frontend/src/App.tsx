0e4a2ec80057946bd8279fa0d2f3b7cd7eb2651c
Philipe Rocha <philiperochad.js@gmail.com>
2021-05-03 10:43:49 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@
 function App() {
   return (
     <div className="App">
-      <h1>ZA WARUUUUDO COME O TOU MARE</h1>
-      <img src="https://static.jojowiki.com/images/thumb/0/0a/latest/20191015213838/DIO_Normal_SC_Infobox_Anime.png/270px-DIO_Normal_SC_Infobox_Anime.png" alt=""/>
+      <h1 className="text-primary">ZA WARUUUUDO COME O TOU MARE</h1>
+      <img src="https://static.jojowiki.com/images/thumb/0/0a/latest/20191015213838/DIO_Normal_SC_Infobox_Anime.png/270px-DIO_Normal_SC_Infobox_Anime.png" alt="" />
     </div>
   );
 }
